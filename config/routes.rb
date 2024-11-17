c49477cd4c531eeb316530867d3fda7ab83c0c04
James Mead <james@floehopper.org>
2024-11-17 21:28:46 +0000

    Fetch audio for use in player on episode page
    The "curl/8.7.1" user agent seems to fix an authorization problem for
    one of the podcasts I'm testing with.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :podcasts, only: [ :index, :show ]
+  resources :episodes, only: [ :show ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 

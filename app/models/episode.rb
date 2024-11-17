c4eaaffe4ec1b193f28be5c7d559878064a8d032
James Mead <james@floehopper.org>
2024-11-17 21:28:46 +0000

    Fetch audio for use in player on episode page
    The "curl/8.7.1" user agent seems to fix an authorization problem for
    one of the podcasts I'm testing with.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,32 @@
+require "open-uri"
+
 class Episode < ApplicationRecord
   belongs_to :podcast
 
+  has_one_attached :audio
+
   validates :guid, presence: true, uniqueness: true
+
+  def fetch
+    return if audio.attached?
+
+    URI.open(enclosure_url, "User-Agent" => "curl/8.7.1") do |io|
+      audio.attach(
+        io: io,
+        filename: filename,
+        content_type: enclosure_type
+      )
+    end
+  rescue OpenURI::HTTPError => e
+    raise "Failed to fetch #{enclosure_url}: #{e.message}"
+  end
+
+  private
+
+  def filename
+    filename = "#{podcast.title.parameterize}_#{title.parameterize}"
+    mime_type = Mime::Type.lookup(enclosure_type)
+    extension = mime_type.symbol.to_s
+    filename.ext(extension)
+  end
 end
